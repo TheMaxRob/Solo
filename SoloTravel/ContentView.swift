d69696a91800ec825f61bb7d222413a55ebda832
Max Roberts <133835313+TheMaxRob@users.noreply.github.com>
2024-04-16 10:35:17 +0100

    Initial Commit

---
+++ SoloTravel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SoloTravel
+//
+//  Created by Max Roberts on 4/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SoloTravel/SoloTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoloTravelApp.swift
+//  SoloTravel
+//
+//  Created by Max Roberts on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SoloTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
