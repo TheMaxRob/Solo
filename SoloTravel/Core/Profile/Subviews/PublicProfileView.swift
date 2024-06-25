35d1e7a082a02eff16d819adb259f0db5dd0f690
Max Roberts <133835313+TheMaxRob@users.noreply.github.com>
2024-06-24 21:14:33 -0400

    Created PublicProfileView, which is what other users will see be brought to when they click on your profile picture thoroughout the app.

---
+++ SoloTravel/Core/Profile/Subviews/PublicProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  PublicProfileView.swift
+//  SoloTravel
+//
+//  Created by Max Roberts on 6/24/24.
+//
+
+import SwiftUI
+
+struct PublicProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PublicProfileView()
+}
