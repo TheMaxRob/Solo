d873ec87bfad5b1f64eb634a43dd0d24c377a643
Max Roberts <133835313+TheMaxRob@users.noreply.github.com>
2025-01-18 17:43:50 -0500

    Meetups now get deleted from cache. Created a LocationManager to try to refactor out some location hijinks that were bloating MapView.

---
+++ SoloTravel/Core/ClusterMapViewRepresentable.swift
@@ -0,0 +1,18 @@
+//
+//  ClusterMapViewRepresentable.swift
+//  SoloTravel
+//
+//  Created by Max Roberts on 1/15/25.
+//
+
+import SwiftUI
+
+struct ClusterMapViewRepresentable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClusterMapViewRepresentable()
+}
